2bf165c7f4669734c4f781d78c59e4acc0c9f83d
thalif <Examsign.thalif@outlook.com>
2022-08-03 11:29:19 +0530

    project init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const port = 3000;
+const app = express();
+var server = app.listen(port, () => {
+    console.log(`App is listening to server on port ${port}`);
+});
+
+app.use(express.static('public'));
+
+//============
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('Yes! Connected id ', socket.id);
+
+    socket.on('chat', function(data)
+    {
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data)
+    {
+        socket.broadcast.emit('typing', data);
+    });
+});
